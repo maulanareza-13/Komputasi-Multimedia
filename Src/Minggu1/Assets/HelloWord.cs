12769d236b5bdfc91bb6bc76123ed385eb5a3ef0
rezabeker <rezabeker8@gmail.com>
2021-02-26 10:08:04 +0700

    src

---
+++ Src/Minggu1/Assets/HelloWord.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWord: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string myText;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+        GUILayout.Label(myText);
+        GUILayout.EndArea();
+    }
+}
