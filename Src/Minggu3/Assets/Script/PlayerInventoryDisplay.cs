ec0eb92c7f637effc073053dc65d304822b42028
rezabeker <rezabeker8@gmail.com>
2021-03-07 07:48:39 +0700

    update

---
+++ Src/Minggu3/Assets/Script/PlayerInventoryDisplay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerInventoryDisplay : MonoBehaviour
+{
+ public Text starText;
+ public void OnChangeCarryingStar(bool carryingStar)
+ {
+ string starMessage = "Tidak Bawa Bintang :-(";
+ if (carryingStar) starMessage = "Bawa Bintang :-)";
+ starText.text = starMessage;
+ }
+}
